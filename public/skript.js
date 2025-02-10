3be993e6baa4bfb177b7fdc09b39e5e60fe102af
housou0401 <seitokai.housou@gmail.com>
2025-02-11 01:03:46 +0900

    Update skript.js

--- public/skript.js
+++ public/skript.js
@@ -13,24 +13,11 @@ function handleSubmit(event) {
 
     submitButton.disabled = true;
     setTimeout(() => { submitButton.disabled = false; }, 10000);
-    messageBox.innerText = "✅送信が完了しました！";
+
+    messageBox.innerHTML = "✅送信が完了しました！<br>リクエストありがとうございました！";
     messageBox.style.color = "green";
+
     event.target.submit();
     responseInput.value = "";
     remarkInput.value = "";
-}
-
-function showAdminLogin() {
-    var password = prompt("⚠️管理者パスワードを入力してください:");
-    if (password) {
-        fetch(`/admin-login?password=${encodeURIComponent(password)}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    window.location.href = "/admin";
-                } else {
-                    alert("⚠️パスワードが間違っています。");
-                }
-            });
-    }
 }
