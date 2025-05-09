dca6a6ddaf7467039aec991f18d1555f0dd9f255
housou0401 <seitokai.housou@gmail.com>
2025-05-09 18:44:16 +0900

    Update server.js

--- server.js
+++ server.js
@@ -490,7 +490,7 @@ app.get("/settings", (req, res) => {
 });
 
 // 20分ごと自動同期
-cron.schedule("*/10 * * * *", async () => {
+cron.schedule("*/8 * * * *", async () => {
   console.log("自動更新ジョブ開始: db.json を GitHub にアップロードします。");
   try {
     await syncRequestsToGitHub();
